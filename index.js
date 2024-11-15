1dc6b9962c222a3fed18c92cbf7baa9b36946403
87LittleDragon <hongnamyip@gmail.com>
2024-11-15 08:33:33 +0800

    fix key path

--- index.js
+++ index.js
@@ -89,8 +89,8 @@ app.get('/successq',(req,res)=>{
 const https = require('https')
 
 const sslServer = https.createServer({
-  key: fs.readFileSync(__dirname + "domain.key"),
-  cert: fs.readFileSync(__dirname + "chained.pem")
+  key: fs.readFileSync(__dirname + "/keys/domain.key"),
+  cert: fs.readFileSync(__dirname + "/keys/chained.pem")
 },app)
 
-sslServer.listen(443, () => console.log('Secure server on port 443'))
+sslServer.listen(443, () => console.log('Secure server on port 443')
